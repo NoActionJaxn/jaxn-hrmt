e30798e5932ad8df29aefd157313eea904121286
NoActionJaxn <jahermitt@gmail.com>
2025-08-18 18:11:18 -0400

    added work page and routing

---
+++ src/constants/dateLocale.ts
@@ -0,0 +1,10 @@
+export const LOCALE = 'en-US';
+
+export const LOCALE_FORMAT_OPTIONS: Intl.DateTimeFormatOptions = {
+  year: 'numeric',
+  month: '2-digit',
+  day: '2-digit',
+  hour: '2-digit',
+  minute: '2-digit',
+  hour12: true, // Enables AM/PM
+};
